cb63ff65f0b41b41b05cc7a1bb1ab2291997061f
Kishor A B <Kishorab023@gmail.com>
2023-02-13 12:33:38 +0530

    Update CamspaceProjectApplication.java

--- src/main/java/com/camspace/CamspaceProject/CamspaceProjectApplication.java
+++ src/main/java/com/camspace/CamspaceProject/CamspaceProjectApplication.java
@@ -3,7 +3,6 @@ package com.camspace.CamspaceProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@SpringBootApplication
 public class CamspaceProjectApplication {
 
 	public static void main(String[] args) {
